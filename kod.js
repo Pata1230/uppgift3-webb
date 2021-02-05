6852fa601320d8bd8f387869f1658b8057aaaa25
Adam Trossing <Adam@trossing.se>
2021-02-05 13:38:48 +0100

    fixat mer

--- kod.js
+++ kod.js
@@ -1,29 +1,94 @@
-let mineFeild = [
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
+
+let mineFeild= [
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
+    [0,0,0,0,0,0],
 ];
-console.table(mineFeild)
+
+function flagcheck() {
+    var checkBox = document.getElementById("flag");
+    var flagga;
+    if (checkBox.checked == true){
+     var flagga = true;
+     console.log("din mamma1")
+    } 
+    else {
+        flagga = false;
+        console.log("din mamma2")
+    }
+  console.log(flagga)
+    return flagga;
+  }
+flagcheck()
+
 
 let mineButtons = document.querySelectorAll(".minebutton");
 console.log(mineButtons)
 
 mineButtons.forEach((button) => {
-
-button.addEventListener("click", OnButtonclick)
-
+    
+    button.addEventListener("click", OnButtonclick)
 })
-    function ResetMineFeild(){
-        
-            for(let y = 0; y<mineFeild.length; y++){
-            for(let x = 0; x< mineFeild[y]; x++){
-                if(Math.random()>0.5){
-                mineFeild[y][x];
-                }
-            }
+SetMineFeild();
+function resetgame (){
+    SetMineFeild();
+    OnButtonclick(e);
+}
+
+function SetMineFeild(){
+    
+    for(let y = 0; y<mineFeild.length; y++){                
+        for(let x = 0; x< mineFeild.length; x++){
+            mineFeild[x][y]=0;
         }
     }
+        for(let y = 0; y<mineFeild.length; y++){                
+            for(let x = 0; x< mineFeild.length; x++){
+            if(Math.random() <= 0.2){ //sätt ut bomber
+                mineFeild[x][y]=1;
+            }
+            else{
+                mineFeild[x][y]=0;
+            }
+         }
+        }
+    }
+
+
+
+function ResetMineFeild(){
+    for(let x =0; x < (mineFeild.length*mineFeild.length); x++){
+        mineButtons[x].disabled = false;
+        mineButtons[x].innerHTML = ' ';
+        mineButtons[x].style.backgroundColor = "gray";
+        neighbouringMines = 0;
+        
+    }
+}
+
+
+
+document.getElementById('button').onclick = function() {
+    ResetMineFeild()
+    for(let y = 0; y<mineFeild.length; y++){                
+        for(let x = 0; x< mineFeild.length; x++){
+            mineFeild[x][y]=0;
+        }
+    }
+    SetMineFeild()
+    console.table(mineFeild)
+
+}
+
+
+
+
+
+console.table(mineFeild)
+
 function OnButtonclick(e){
     let coordiante = e.target.value.split(",");
     console.log(coordiante);
@@ -33,10 +98,10 @@ function OnButtonclick(e){
     //kolla åvan
     //south
     if(checkBounds(x, y-1)){
-    if(mineFeild[y-1][x]===1){
-        neighbouringMines++;
+        if(mineFeild[y-1][x]===1){
+            neighbouringMines++;
+        }
     }
-}
     //north
     if(checkBounds(x, y+1)){
     if(mineFeild[y+1][x]===1){
@@ -79,11 +144,17 @@ if(checkBounds(x-1, y+1)){
         neighbouringMines++;
     }
 }
-
+flagcheck();
+if(flagga == true){
+    e.target.mineButtons.innerHTML = '🚩'
+}
+else{
     if (mineFeild[y][x] == 1){
         console.log("you hit a mine")
         e.target.style.backgroundColor="#ff0000";
         e.target.disabled = true;
+       
+
     }
     else{
         if (neighbouringMines > 0){
@@ -94,7 +165,7 @@ if(checkBounds(x-1, y+1)){
         console.log("no mine")
     }
 }
-
+}
 function checkBounds(x,y){
     if(y>=0 && y <= mineFeild.length-1){
         if(x>=0 && x <= mineFeild[y].length -1){
